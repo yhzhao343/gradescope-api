d68abe8c4ee7d84554510b71e9d5f951ea49cdfe
Kevin <67246199+KevZhe@users.noreply.github.com>
2024-04-20 16:34:29 -0400

    updated main to have a tiny demo for get_courses()

--- src/sylveon/__main__.py
+++ src/sylveon/__main__.py
@@ -2,6 +2,9 @@
 
 from sylveon._hello._greeting import hello_world
 from sylveon.string_length.find_length import find_length
+from sylveon._classes._connection import GSConnection
+from dotenv import load_dotenv
+import os
 
 
 def main():
@@ -9,6 +12,20 @@ def main():
     print(greeting)
     length_str = find_length(greeting)
     print(f"Length of string {greeting} is {length_str}")
+
+    connection = GSConnection()
+
+    # load .env file
+    load_dotenv()
+
+    # login
+    connection.login(os.getenv("EMAIL"), os.getenv("PASSWORD"))
+    account = connection.account
+
+    # get courses
+    courses = account.get_courses()
+
+    print(courses)
 
 
 if __name__ == "__main__":
