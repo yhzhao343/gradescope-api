88e2ceec5c1ba67f275067c0d497ca8623ea5ded
Calvin <48587962+calvinatian@users.noreply.github.com>
2024-04-22 23:26:00 -0400

    Remove unneeded files from template

--- tests/test_string_length.py
+++
@@ -1,9 +0,0 @@
-# Some basic test for string length
-
-from sylveon.string_length.find_length import find_length
-
-def test_string_length_succeed():
-    string = "Hello World!"
-    l = find_length(string)
-
-    assert l == len(string)
