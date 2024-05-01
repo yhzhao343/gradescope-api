55ad30d479f0dbb393f3caab8bcf33600321bd95
Kevin Zheng <kz1252@nyu.edu>
2024-05-01 16:14:08 -0400

    revoked changes to main

--- src/sylveon/__main__.py
+++ src/sylveon/__main__.py
@@ -1,31 +1,8 @@
 # Example showing how to import different components of the package and use them in the main function
 
-from sylveon._hello._greeting import hello_world
-from sylveon.string_length.find_length import find_length
-from sylveon._classes._connection import GSConnection
-from dotenv import load_dotenv
-import os
-
 
 def main():
-    greeting = hello_world()
-    print(greeting)
-    length_str = find_length(greeting)
-    print(f"Length of string {greeting} is {length_str}")
-
-    connection = GSConnection()
-
-    # load .env file
-    load_dotenv()
-
-    # login
-    connection.login(os.getenv("EMAIL"), os.getenv("PASSWORD"))
-    account = connection.account
-
-    # get courses
-    courses = account.get_courses()
-
-    print(courses)
+    pass
 
 
 if __name__ == "__main__":
