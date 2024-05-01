5cced3ef4a1f216bae37da84ba36c0adbbc404d2
Calvin <48587962+calvinatian@users.noreply.github.com>
2024-05-01 13:25:06 -0400

    Add initial get extensions

--- src/sylveon/_classes/_extensions.py
+++ src/sylveon/_classes/_extensions.py
@@ -1,8 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
-from dataclasses import dataclass
-import datetime
-
 """
 Functions for getting, adding, and changing student extensions on assignments.
 get_extensions()
@@ -11,13 +6,21 @@ update_student_late_due_date()
 update_student_release_date()
 """
 
+import requests
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+import datetime
+import json
+import zoneinfo
+
 
 @dataclass
 class Extension:
-    # user_id: str
+    name: str
     release_date: datetime.datetime
     due_date: datetime.datetime
     late_due_date: datetime.datetime
+    delete_path: str
 
 
 def get_extensions(
@@ -25,14 +28,21 @@ def get_extensions(
 ) -> dict:
     """
     Get all extensions for an assignment.
-    {
-        user_id: Extension,
-        user_id: Extension,
-        ...
-    }
+
+    Args:
+        session (requests.Session): The session object used for making HTTP requests.
+        course_id (str): The ID of the course.
+        assignment_id (str): The ID of the assignment.
+
+    Returns:
+        dict: A dictionary containing the extensions, where the keys are user IDs and the values are Extension objects.
+
+    Raises:
+        Exception: If the request to get extensions fails.
+
     """
     GS_EXTENSIONS_ENDPOINT = f"https://www.gradescope.com/courses/{course_id}/assignments/{assignment_id}/extensions"
-    GS_EXTENSIONS_TABLE_CSS_ID = "DataTables_Table_0"
+    GS_EXTENSIONS_TABLE_CSS_CLASSES = "table js-overridesTable"  # Table
 
     # get the extensions from the page
     response = session.get(GS_EXTENSIONS_ENDPOINT)
@@ -46,12 +56,69 @@ def get_extensions(
     # parse the html response
     extensions_soup = BeautifulSoup(response.text, "html.parser")
 
-    extensions_table = extensions_soup.find("table", id=GS_EXTENSIONS_TABLE_CSS_ID)
     extensions_table = extensions_soup.find(
-        "table", attrs={"id": GS_EXTENSIONS_TABLE_CSS_ID}
+        "table", class_=GS_EXTENSIONS_TABLE_CSS_CLASSES
     )
+    table_head = extensions_table.find("thead").find("tr")
+    for heading in table_head.find_all("th"):
+        print(f"'{heading.text}'")
 
     extensions = {}
+
+    table_body = extensions_table.find("tbody")
+    for row in table_body.find_all("tr"):
+        """
+        '{"inheritedOverride":false,"deletePath":"/courses/753413/assignments/4330410/extensions/919001","hideEmailAddresses":false,"override":{"user_id":6515875,"settings":{"due_date":{"type":"absolute","value":"2024-04-15T20:00"},"release_date":{"type":"absolute","value":"2024-04-14T20:00"},"hard_due_date":{"type":"absolute","value":"2024-04-16T20:00"}}},"path":"/courses/753413/assignments/4330410/extensions","resourceName":"test","studentName":"Gradescope API - CI Student Testing Account","studentSections":null,"userId":6515875,"fallbackSubmissionWindow":{"release_date":"2024-04-10T17:00","due_date":"2024-04-30T18:00","hard_due_date":null,"time_limit":null,"visible":true},"timezone":{"abbr":"EDT","zone":"Eastern Time (US \\u0026 Canada)","identifier":"America/New_York"},"assignment":{"type":"ProgrammingAssignment","time_limit":null,"release_date":"2024-04-10T17:00","due_date":"2024-04-30T18:00","hard_due_date":null,"visible":true}}'
+        """
+        user_properties = row.find("div", {"data-react-class": "EditExtension"}).get(
+            "data-react-props"
+        )
+        user_properties = json.loads(user_properties)
+
+        # user id
+        user_id = str(user_properties["override"]["user_id"])  # TODO: keep as int?
+
+        # timezone
+        timezone = zoneinfo.ZoneInfo(user_properties["timezone"]["identifier"])
+
+        # extension properties
+        extension_info = user_properties["override"]["settings"]
+        release_date = extension_info.get("release_date", {}).get("value", None)
+        due_date = extension_info.get("due_date", {}).get("value", None)
+        late_due_date = extension_info.get("hard_due_date", {}).get("value", None)
+
+        # convert dates to datetime objects
+        release_date = (
+            datetime.datetime.fromisoformat(release_date).replace(tzinfo=timezone)
+            if release_date
+            else None
+        )
+        due_date = (
+            datetime.datetime.fromisoformat(due_date).replace(tzinfo=timezone)
+            if due_date
+            else None
+        )
+        late_due_date = (
+            datetime.datetime.fromisoformat(late_due_date).replace(tzinfo=timezone)
+            if late_due_date
+            else None
+        )
+
+        # delete path
+        delete_path = user_properties["deletePath"]
+
+        # name
+        name = user_properties["studentName"]
+
+        # create extension object
+        extension = Extension(
+            name=name,
+            release_date=release_date,
+            due_date=due_date,
+            late_due_date=late_due_date,
+            delete_path=delete_path,
+        )
+        extensions[user_id] = extension
 
     # return the extensions
     return extensions
@@ -64,7 +131,7 @@ def update_student_extension(
     user_id: str,
     release_date: datetime.datetime | None = None,
     due_date: datetime.datetime | None = None,
-    late_due_date: datetime.datetime | None= None,
+    late_due_date: datetime.datetime | None = None,
 ) -> bool:
     """Updates the extension for a student on an assignment
 
@@ -75,7 +142,7 @@ def update_student_extension(
 
     Requirements:
         release_date <= due_date <= late_due_date
-    
+
     Notes:
         Extensions can go "backwards" too. For example, a user can have
         a release date earlier than the normal release date or a due date
@@ -96,9 +163,13 @@ def update_student_extension(
         raise ValueError("At least one date must be provided")
 
     # Check if date requirements are met (in order)
-    dates = [date for date in [release_date, due_date, late_due_date] if date is not None]
+    dates = [
+        date for date in [release_date, due_date, late_due_date] if date is not None
+    ]
     if dates != sorted(dates):
-        raise ValueError("Dates must be in order: release_date <= due_date <= late_due_date")
+        raise ValueError(
+            "Dates must be in order: release_date <= due_date <= late_due_date"
+        )
 
     def add_to_body(extension_name: str, extension_datetime: datetime.datetime):
         """Update JSON body for POST request"""
--- tests/test_extension.py
+++ tests/test_extension.py
@@ -9,6 +9,8 @@ from sylveon._classes._login_helpers import (
     get_auth_token_init_gradescope_session,
 )
 
+from sylveon._classes._connection import GSConnection
+
 from sylveon._classes._extensions import get_extensions, update_student_extension
 
 # load .env file
@@ -20,18 +22,43 @@ GRADESCOPE_CI_INSTRUCTOR_EMAIL = os.getenv("GRADESCOPE_CI_INSTRUCTOR_EMAIL")
 GRADESCOPE_CI_INSTRUCTOR_PASSWORD = os.getenv("GRADESCOPE_CI_INSTRUCTOR_PASSWORD")
 
 
-@pytest.mark.skip("Not implemented")
+def new_session(account_type="student"):
+    """Creates and returns a session for testing"""
+    connection = GSConnection()
+
+    match account_type.lower():
+        case "student":
+            connection.login(
+                GRADESCOPE_CI_STUDENT_EMAIL, GRADESCOPE_CI_STUDENT_PASSWORD
+            )
+        case "instructor":
+            connection.login(
+                GRADESCOPE_CI_INSTRUCTOR_EMAIL, GRADESCOPE_CI_INSTRUCTOR_PASSWORD
+            )
+        case _:
+            raise ValueError("Invalid account type: must be 'student' or 'instructor'")
+
+    return connection.session
+
+
+# @pytest.mark.skip("Not implemented")
 def test_get_extensions():
     # create test session
     test_session = requests.Session()
 
-    # call the function
+    # assuming test_get_auth_token_init_gradescope_session works
     auth_token = get_auth_token_init_gradescope_session(test_session)
+    login_check = login_set_session_cookies(
+        test_session,
+        GRADESCOPE_CI_INSTRUCTOR_EMAIL,
+        GRADESCOPE_CI_INSTRUCTOR_PASSWORD,
+        auth_token,
+    )
 
-    # check cookies
-    cookies = requests.utils.dict_from_cookiejar(test_session.cookies)
-    cookie_check = set(cookies.keys()) == {"_gradescope_session"}
-    assert auth_token and cookie_check
+    course_id = "753413"
+    assignment_id = "4330410"
+    extensions = get_extensions(test_session, course_id, assignment_id)
+    print(extensions)
 
 
 def test_valid_change_extension():
