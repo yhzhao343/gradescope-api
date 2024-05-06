123fe5b74e9bd8c1d1555df98eac8dfbce4bc340
Calvin <48587962+calvinatian@users.noreply.github.com>
2024-05-06 00:55:28 -0400

    Refactor upload_assignment function to improve readability and return url to submission

--- src/sylveon/_classes/_upload.py
+++ src/sylveon/_classes/_upload.py
@@ -1,9 +1,7 @@
-"""Functions for uploading assignments to Gradescope.
-"""
+"""Functions for uploading assignments to Gradescope."""
 
 import requests
 from bs4 import BeautifulSoup
-from dataclasses import dataclass
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import mimetypes
 import io
@@ -15,25 +13,37 @@ def upload_assignment(
     course_id: str,
     assignment_id: str,
     *files: io.TextIOWrapper,
-):
-    """
-    Upload an assignment to Gradescope.
+    leaderboard_name: str | None = None,
+) -> str | None:
+    """Uploads given file objects to the specified assignment on Gradescope.
+
+    Args:
+        session (requests.Session): The session object to use for making HTTP requests.
+        course_id (str): The ID of the course on Gradescope.
+        assignment_id (str): The ID of the assignment on Gradescope.
+        *files (io.TextIOWrapper): Variable number of file objects to upload.
+        leaderboard_name (str | None, optional): The name of the leaderboard. Defaults to None.
+
+    Returns:
+        str | None: Link to submission if successful or None if unsuccessful.
     """
     GS_COURSE_ENDPOINT = f"https://www.gradescope.com/courses/{course_id}"
     GS_UPLOAD_ENDPOINT = f"https://www.gradescope.com/courses/{course_id}/assignments/{assignment_id}/submissions"
 
     # Get auth token
+    # TODO: Refactor to helper function since it is needed in multiple places
     response = session.get(GS_COURSE_ENDPOINT)
     soup = BeautifulSoup(response.text, "html.parser")
     auth_token = soup.find("meta", {"name": "csrf-token"})["content"]
 
+    # Format files for upload
     form_files = []
     for file in files:
         form_files.append(
             (
                 "submission[files][]",
                 (
-                    pathlib.Path(file.name).name,
+                    pathlib.Path(file.name).name,  # get the filename from the path
                     file,
                     mimetypes.guess_type(file.name)[0],
                 ),
@@ -41,15 +51,16 @@ def upload_assignment(
         )
 
     # Setup multipart form data
-    multipart = MultipartEncoder(
-        fields=(
-            ("utf8", "✓"),
-            ("authenticity_token", auth_token),
-            ("submission[method]", "upload"),
-            ("submission[leaderboard_name]", "test"),
-            *form_files,
-        )
-    )
+    fields = [
+        ("utf8", "✓"),
+        ("authenticity_token", auth_token),
+        ("submission[method]", "upload"),
+        *form_files,
+    ]
+    if leaderboard_name is not None:
+        fields.append(("submission[leaderboard_name]", leaderboard_name))
+
+    multipart = MultipartEncoder(fields=fields)
 
     headers = {
         "Content-Type": multipart.content_type,
@@ -57,4 +68,7 @@ def upload_assignment(
     }
     response = session.post(GS_UPLOAD_ENDPOINT, data=multipart, headers=headers)
 
-    return response.status_code == 200
+    # Note: Response status code is always 200 even if upload was unsuccessful (e.g. past the due date,
+    # missing form fields, etc.). The response from the server either redirects to the submission page (url)
+    # if successful, or redirects to the Course homepage if unsuccessful.
+    return None if response.url == GS_COURSE_ENDPOINT or response.url.endswith("submissions") else response.url
--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,8 +1,5 @@
-import requests
 import os
 from dotenv import load_dotenv
-import pytest
-from datetime import datetime, timedelta
 
 from sylveon._classes._connection import GSConnection
 
@@ -36,12 +33,12 @@ def new_session(account_type="student"):
     return connection.session
 
 
-def test_upload():
+def test_valid_upload():
     # create test session
     test_session = new_session("student")
 
     course_id = "753413"
-    assignment_id = "4330410"
+    assignment_id = "4455030"
 
     with (
         open("tests/upload_files/text_file.txt", "rb") as text_file,
@@ -49,13 +46,39 @@ def test_upload():
         open("tests/upload_files/python_file.py", "rb") as python_file,
     ):
 
-        extensions = upload_assignment(
+        submission_link = upload_assignment(
             test_session,
             course_id,
             assignment_id,
             text_file,
             markdown_file,
             python_file,
+            leaderboard_name="test",
         )
 
-    assert extensions
+    assert submission_link is not None
+
+
+def test_invalid_upload():
+    # create test session
+    test_session = new_session("student")
+
+    course_id = "753413"
+    invalid_assignment_id = "1111111"
+
+    with (
+        open("tests/upload_files/text_file.txt", "rb") as text_file,
+        open("tests/upload_files/markdown_file.md", "rb") as markdown_file,
+        open("tests/upload_files/python_file.py", "rb") as python_file,
+    ):
+
+        submission_link = upload_assignment(
+            test_session,
+            course_id,
+            invalid_assignment_id,
+            text_file,
+            markdown_file,
+            python_file,
+        )
+
+    assert submission_link is None
