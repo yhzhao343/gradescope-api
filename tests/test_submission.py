d4abea2850cfc4b02b13a3f94a1d81445104d127
Calvin <48587962+calvinatian@users.noreply.github.com>
2025-01-04 13:37:13 -0500

    Add test for getting student assignments

--- tests/test_submission.py
+++ tests/test_submission.py
@@ -3,8 +3,8 @@ import os
 import pytest
 from dotenv import load_dotenv
 
+from gradescopeapi.classes.account import Account
 from gradescopeapi.classes.assignments import Assignment
-from gradescopeapi.classes.connection import GSConnection
 
 # Load .env file
 load_dotenv()
@@ -13,21 +13,9 @@ GRADESCOPE_CI_INSTRUCTOR_PASSWORD = os.getenv("GRADESCOPE_CI_INSTRUCTOR_PASSWORD
 GRADESCOPE_CI_STUDENT_EMAIL = os.getenv("GRADESCOPE_CI_STUDENT_EMAIL")
 
 
-def get_account(account_type="instructor"):
-    """Creates a connection and returns the account for testing"""
-    connection = GSConnection()
-    if account_type == "instructor":
-        connection.login(
-            GRADESCOPE_CI_INSTRUCTOR_EMAIL, GRADESCOPE_CI_INSTRUCTOR_PASSWORD
-        )
-    else:
-        raise ValueError("Invalid account type: must be 'instructor'")
-    return connection.account
-
-
-def test_get_assignments():
-    """Test fetching assignments with valid course ID."""
-    account = get_account("instructor")
+def test_get_assignments_instructor(create_session):
+    """Test fetching assignments with valid course ID as an instructor."""
+    account = Account(create_session("instructor"))
     course_id = "753413"
     assignments = account.get_assignments(course_id)
     assert isinstance(assignments, list), "Should return a list of assignments"
@@ -37,9 +25,21 @@ def test_get_assignments():
     ), "All items should be Assignment instances"
 
 
-def test_get_assignment_submissions():
+def test_get_assignments_student(create_session):
+    """Test fetching assignments with valid course ID as a student."""
+    account = Account(create_session("student"))
+    course_id = "753413"
+    assignments = account.get_assignments(course_id)
+    assert isinstance(assignments, list), "Should return a list of assignments"
+    assert len(assignments) > 0, "Should contain at least 1 assignment"
+    assert all(
+        isinstance(a, Assignment) for a in assignments
+    ), "All items should be Assignment instances"
+
+
+def test_get_assignment_submissions(create_session):
     """Test fetching assignment submissions with valid course and assignment IDs."""
-    account = get_account("instructor")
+    account = Account(create_session("instructor"))
     course_id = "753413"
     assignment_id = "4330410"
 
@@ -51,9 +51,9 @@ def test_get_assignment_submissions():
     ), "Each submission ID should map to a list of links"
 
 
-def test_get_assignment_submission_valid():
+def test_get_assignment_submission_valid(create_session):
     """Test fetching a specific assignment submission."""
-    account = get_account("instructor")
+    account = Account(create_session("instructor"))
     student_email = GRADESCOPE_CI_STUDENT_EMAIL
     course_id = "753413"
     assignment_id = "4330410"
@@ -66,9 +66,9 @@ def test_get_assignment_submission_valid():
     assert len(submission) == expected_num_files, "List should contain aws links"
 
 
-def test_get_assignment_submission_no_submission_found():
+def test_get_assignment_submission_no_submission_found(create_session):
     """Test case when no submission is found for a given student."""
-    account = get_account("instructor")
+    account = Account(create_session("instructor"))
     student_email = GRADESCOPE_CI_INSTRUCTOR_EMAIL
     course_id = "753413"
     assignment_id = "5525291"
