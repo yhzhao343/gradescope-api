c6bc6d5c0b2b3bbc9dbf6593761522d965c19a1d
Calvin <48587962+calvinatian@users.noreply.github.com>
2025-11-22 03:46:46 -0500

    feat: add function to update docker image name

--- tests/test_edit_assignment.py
+++ tests/test_edit_assignment.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
 
-from gradescopeapi.classes.assignments import update_assignment_date
+from gradescopeapi.classes.assignments import (
+    update_assignment_date,
+    update_autograder_image_name,
+)
 
 
 def test_valid_change_assignment(create_session):
@@ -42,3 +45,37 @@ def test_boundary_date_assignment(create_session):
         boundary_date,
     )
     assert result, "Failed to update assignment with boundary dates"
+
+
+def test_autograder_valid_image_name(create_session):
+    """Test updating assignment with boundary date values."""
+    test_session = create_session("instructor")
+
+    course_id = "753413"
+    assignment_id = "7193007"
+    image_name = "gradescope/autograder-base:ubuntu-22.04"
+
+    result = update_autograder_image_name(
+        test_session,
+        course_id,
+        assignment_id,
+        image_name,
+    )
+    assert result, "Failed to update autograder image name"
+
+
+def test_autograder_invalid_image_name(create_session):
+    """Test updating assignment with boundary date values."""
+    test_session = create_session("instructor")
+
+    course_id = "753413"
+    assignment_id = "7193007"
+    image_name = "gradescope/autograders:us-prod-docker_image-123456"
+
+    result = update_autograder_image_name(
+        test_session,
+        course_id,
+        assignment_id,
+        image_name,
+    )
+    assert not result, "Incorrectly updated to invalid autograder image name"
